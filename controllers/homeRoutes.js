bf3c3bfdee2145e860b58f64aa9b7c3c89e659bb
brianmyer <myer.brian@gmail.com>
2023-09-06 12:53:06 -0400

    handlbars initial structures

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,7 +3,7 @@ const { Poll, User } = require('../models');
 const withAuth = require('../utils/auth');
 
 
-router.get('*', async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     // Get all projects and JOIN with user data
     const projectData = await Poll.findAll({
@@ -80,4 +80,13 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
+router.get('/signup', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+
+  res.render('signup');
+});
 module.exports = router;
