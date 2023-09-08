8fed3ad7d87f7f72af96612d516d47148a91ba8b
brianmyer <myer.brian@gmail.com>
2023-09-08 10:36:00 -0400

    committing for git pull

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -84,16 +84,6 @@ router.get('/create-responses/:id', async (req, res) => {
   }
 });
 
-router.get('/homepage', async (req, res) => {
-  try {
-
-    res.render('homepage', { 
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
 router.get('/response/:id', async (req, res) => {
   try {
     const pollData = await Poll.findOne( { 
@@ -131,26 +121,6 @@ router.get('/results/:id', async (req, res) => {
     res.status(500).json(err);
   }
 });
-
-// // Use withAuth middleware to prevent access to route
-// router.get('/profile', withAuth, async (req, res) => {
-//   try {
-//     // Find the logged in user based on the session ID
-//     const userData = await User.findByPk(req.session.user_id, {
-//       attributes: { exclude: ['password'] },
-//       include: [{ model: Project }],
-//     });
-
-//     const user = userData.get({ plain: true });
-
-//     res.render('profile', {
-//       ...user,
-//       logged_in: true
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
 
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
